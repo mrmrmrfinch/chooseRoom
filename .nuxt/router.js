e9236e79adc3e14d932d3063602b23214c30206a
Colin Zang <zyy0116@gmail.com>
2022-04-07 18:36:26 -0400

    new commit

---
+++ .nuxt/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _2e88765a = () => interopDefault(import('../pages/questions.vue' /* webpackChunkName: "pages/questions" */))
+const _3d561e95 = () => interopDefault(import('../pages/reverb.vue' /* webpackChunkName: "pages/reverb" */))
+const _25e5d5df = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/questions",
+    component: _2e88765a,
+    name: "questions"
+  }, {
+    path: "/reverb",
+    component: _3d561e95,
+    name: "reverb"
+  }, {
+    path: "/",
+    component: _25e5d5df,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <v-main>
-    <v-container>
-    <h1>Acoustics Portfolio Demo</h1>
-    <a href="/questions"> Questions Demo </a>
-    <br />
-    <a href="/reverb"> Reverb Demo </a>
+  <v-main style="height: 100vh">
+    <v-container style="height: 100%">
+      <div class="center">
+        <h1
+          style="
+            font-size: 5rem;
+            font-weight: 600;
+            color: white;
+            text-shadow: 2px 2px 14px rgba(0, 0, 0, 0.2);
+          "
+        >
+          An Acoustically-Accurate<br />Room Selector Tool.
+        </h1>
+        <p style="color: white; padding-bottom: 20px">
+          A Project from University of Rochester AME292, 2022 Spring.
+        </p>
+        <v-btn elevation="2" x-large onclick="window.location='/questions'">
+          Questions Demo</v-btn
+        >
+        <v-btn elevation="2" x-large onclick="window.location='/reverb'">
+          Reverb Demo</v-btn
+        >
+      </div>
     </v-container>
   </v-main>
 </template>
@@ -14,3 +31,47 @@ export default {
   name: "indexPage",
 };
 </script>
+
+<style scope>
+@font-face {
+  font-family: "Manrope";
+  src: url("/manrope.woff2") format("woff2");
+}
+
+body {
+  background: linear-gradient(
+    -55deg,
+    #45858c,
+    #a3d9d9,
+    #d9c589,
+    #bf9765,
+    #d92b04
+  );
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  height: 100vh;
+  font-family: "Manrope";
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+.center {
+  margin: 0;
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+</style>
